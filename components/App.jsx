c406b07348830934e88d4e541359bafd7189ca3d
David Solinger <solinger10@gmail.com>
2017-02-07 20:36:15 -0500

    progress

--- components/App.jsx
+++ components/App.jsx
@@ -15,7 +15,7 @@ function App({ children, routes }) {
   return (
     <div>
   <header>
-      <h1><title>When should I leave for the airport?</title></h1>
+  <h1>When should I leave for the airport?</h1>
 </header>
       {children}
       <div style={{ color: '#A0A0A0', fontSize: '14px', marginTop: '50px', textAlign: 'center'}}>
